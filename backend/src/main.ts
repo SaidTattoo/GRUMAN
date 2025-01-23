f88999f7d28a7166ba0107c9acaee521e73dd81c
SaidRavestG <sravest@sigmasa.cl>
2025-01-23 14:02:35 -0300

    fix errores de cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,19 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   // Configuración de CORS
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:8100',
-      'capacitor://localhost',
-      'http://localhost',
-      'https://tudominio.com',
-      'capacitor://tudominio.com'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
